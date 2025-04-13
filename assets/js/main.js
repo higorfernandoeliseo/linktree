dbf0abb21721df5a4d1407fe912cb493f209122e
Igor Eliseo <higorfernandoeliseo2016@gmail.com>
2025-04-13 17:47:49 -0300

    finalizado o conecito do site

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ function makevCard(){
     var vcard = "BEGIN:VCARD\nVERSION:3.0\n"
         + "N:"+nome.innerText+"\n"
         + "FN:"+nome.innerText+"\n"
-        + "TEL;TYPE=CELL:5516997049362\n"
+        + "TEL;TYPE=CELL:5519886756945\n"
         + "PHOTO;TYPE=JPEG;ENCODING=b:"+image.value+"\n"
         + "END:VCARD";
 
